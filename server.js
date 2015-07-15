84e553f94325e0bf46889bbbbdb29f8e913b9dc7
Saren <saren@wtako.net>
2015-07-16 00:07:09 +0800

    idk

--- server.js
+++ server.js
@@ -133,7 +133,7 @@ var makeID = function() {
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   for (var i = 0; i < 32; i++)
-  text += possible.charAt(Math.floor(Math.random() * possible.length));
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
 
   return text;
 }
